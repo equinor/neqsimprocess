9d8258a85cb83e2b16e5094e08fcf81b91703c58
Even Solbraa <esol@equinor.com>
2020-05-07 14:58:25 +0200

    startup

---
+++ example/glycolExample1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 14:56:27 2020
+
+@author: ESOL
+"""
+
+print('example of process condition monitor....')
+
--- src/neqsim-condition-monitoring/glycolprocess.py
+++ src/neqsim-condition-monitoring/glycolprocess.py
@@ -7,3 +7,5 @@ Created on Thu May  7 14:49:44 2020
 
 import neqsim
 
+print('start setting up glycol process...')
+

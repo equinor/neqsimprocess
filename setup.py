20b17b2e9a8bc41c1fd8b31e446615905e8bd999
Even Solbraa <ESOL@equinor.com>
2020-08-13 17:28:54 +0200

    updates

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="neqsim-condition-monitoring",
+    name="neqsimprocess",
     version="0.1",
     author="Even Solbraa","Marlene Louisen Lund", "Knut Maråk"
     author_email="esolbraa@gmail.com",
     description="Process condition monitoring using NeqSim",
     long_description="Process condition monitoring using NeqSim",
     long_description_content_type="text/markdown",
-    url="https://github.com/equinor/neqsim-condition-monitoring",
-    packages=['neqsim-condition-monitoring'],
-    package_dir={'neqsim-condition-monitoring': 'src/neqsim-condition-monitoring'},
+    url="https://github.com/equinor/neqsimprocess",
+    packages=['neqsimprocess'],
+    package_dir={'neqsimprocess': 'src/neqsimprocess'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",

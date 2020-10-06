72a92894eb64df375909141d690d72e836f98101
Even Solbraa <esol@equinor.com>
2020-10-06 10:31:35 +0200

    added TEG process including heat exchangers and TEG buffer tank

--- example/TEGprocessHX.py
+++ example/TEGprocessHX.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+e# -*- coding: utf-8 -*-
 """
 Created on Wed Jul 15 11:07:21 2020
 
@@ -11,7 +11,7 @@ import time
 # Read dehydration process from file
 #offshorePro = openprocess('c:/temp/offshorePro.neqsim')
 #TEGprocess = openprocess('../lib/TEGprocess.neqsim')
-TEGprocess = openprocess('c:/temp//TEGprocess.neqsim')
+TEGprocess = openprocess('c:/temp//TEGprocessHeatEx.neqsim')
 #TEGprocess.setFluid(TEGprocess.getUnit("dry feed gas").getFluid(), offshorePro.getUnit("richgas").getFluid())
 
 
@@ -21,11 +21,9 @@ TEGprocess.getUnit("dry feed gas").setTemperature(30.4, 'C')
 TEGprocess.getUnit("dry feed gas").setPressure(52.21, "bara")
 TEGprocess.getUnit("lean TEG to absorber").setFlowRate(6862.5, "kg/hr")
 TEGprocess.getUnit("Rich TEG HP flash valve").setOutletPressure(4.9)
-TEGprocess.getUnit("rich TEG heater HP").setOutTemperature(273.15 + 62.0)
-TEGprocess.getUnit("LP rich glycol heater").setOutTemperature(273.15 + 139.0)
-TEGprocess.getUnit("LP rich glycol heater").setOutPressure(1.23)
-TEGprocess.getUnit("TEG regeneration column").getCondenser().setOutTemperature(273.15 + 110.3)
-TEGprocess.getUnit("TEG regeneration column").getReboiler().setOutTemperature(273.15 + 205.6)
+TEGprocess.getUnit("Rich TEG LP flash valve").setOutletPressure(1.23)
+TEGprocess.getUnit("TEG regeneration column").getCondenser().setOutTemperature(273.15 + 100.0)
+TEGprocess.getUnit("TEG regeneration column").getReboiler().setOutTemperature(273.15 + 206.6)
 TEGprocess.getUnit("TEG regeneration column").setBottomPressure(1.23)
 TEGprocess.getUnit("TEG regeneration column").setTopPressure(1.2)
 TEGprocess.getUnit("regen gas cooler").setOutTemperature(273.15+37.5)
@@ -34,10 +32,8 @@ TEGprocess.getUnit("stripGas").setFlowRate(90.0, "Sm3/hr")
 TEGprocess.getUnit("stripGas").setTemperature(80.0, 'C')
 TEGprocess.getUnit("TEG buffer tank").setOutTemperature(273.15+185.0)
 TEGprocess.getUnit("hot lean TEG pump").setOutletPressure(20.0)
-TEGprocess.getUnit("hot lean TEG pump").setIsentropicEfficiency(0.75)
 TEGprocess.getUnit("lean TEG cooler").setOutTemperature(273.15+43.0)                            
-TEGprocess.getUnit("lean TEG HP pump").setOutletPressure(52.21)                          
-TEGprocess.getUnit("lean TEG HP pump").setIsentropicEfficiency(0.75)
+TEGprocess.getUnit("lean TEG HP pump").setOutletPressure(52.21)
 #Run the process calculations
 tic = time.perf_counter()
 TEGprocess.run()
@@ -49,7 +45,9 @@ wetGasRatekghr = TEGprocess.getUnit("water saturated feed gas").getFlowRate("kg/
 waterInWetGasppm = TEGprocess.getUnit("water saturated feed gas").getFluid().getPhase(0).getComponent('water').getz()*1.0e6
 waterInDryGasppm = TEGprocess.getUnit("dry gas from absorber").getFluid().getPhase(0).getComponent('water').getz()*1.0e6
 waterInWetGaskgMSm3 = waterInWetGasppm*0.01802*101325.0/(8.314*288.15)
-waterInWetGaskghr = waterInWetGaskgMSm3*TEGprocess.getUnit("dry feed gas").getFlowRate("Sm3/day")/1.0e6/24.0
+waterInDryGaskgMSm3 = waterInDryGasppm*0.01802*101325.0/(8.314*288.15)
+waterInWetGaskghr = waterInWetGaskgMSm3*TEGprocess.getUnit("water saturated feed gas").getFlowRate("Sm3/day")/1.0e6/24.0
+waterInDryGaskghr = waterInDryGaskgMSm3*TEGprocess.getUnit("dry gas from absorber").getFlowRate("Sm3/day")/1.0e6/24.0
 waterInDryGaskgMSm3 = waterInDryGasppm*0.01802*101325.0/(8.314*288.15)
 wtLeanTEGFromReboiler = TEGprocess.getUnit("TEG regeneration column").getLiquidOutStream().getFluid().getPhase("aqueous").getWtFrac("TEG")*100.0
 wtLeanTEGFromStripper = TEGprocess.getUnit("TEG stripper").getSolventOutStream().getFluid().getPhase("aqueous").getWtFrac("TEG")*100.0
@@ -61,7 +59,6 @@ richTEGtemperatureAfterDepres = TEGprocess.getUnit("Rich TEG HP flash valve").ge
 RichTEGwtprbinary = TEGprocess.getUnit("TEG absorber").getSolventOutStream().getFluid().getPhase("aqueous").getWtFrac("TEG")/(TEGprocess.getUnit("TEG absorber").getSolventOutStream().getFluid().getPhase("aqueous").getWtFrac("TEG")+TEGprocess.getUnit("TEG absorber").getSolventOutStream().getFluid().getPhase("aqueous").getWtFrac("water"))*100
 reboilerdutykW = TEGprocess.getUnit("TEG regeneration column").getReboiler().getDuty()/1.0e3
 condenserdutykW = TEGprocess.getUnit("TEG regeneration column").getCondenser().getDuty()/1.0e3
-glycolheater2dutykW = TEGprocess.getUnit("rich TEG heater HP").getDuty()/1.0e3 
 condensertemperature= TEGprocess.getUnit("TEG regeneration column").getCondenser().getTemperature()-273.15
 reboilertemperature= TEGprocess.getUnit("TEG regeneration column").getReboiler().getTemperature()-273.15
 TEGmakeupkghr = TEGprocess.getUnit("makeup calculator").getOutputVariable().getFluid().getFlowRate("kg/hr")
@@ -78,6 +75,11 @@ TEGinwatertoTreatmentwtprecent = TEGprocess.getUnit("water to treatment").getFlu
 flowFromReboiler = TEGprocess.getUnit("TEG regeneration column").getReboiler().getLiquidOutStream().getFlowRate("kg/hr")
 richTEGpreheaterDutykW = TEGprocess.getUnit("rich TEG preheater").getDuty()/1000.0
 richTEGpreheaterTemperature = TEGprocess.getUnit("rich TEG preheater").getOutStream().getTemperature("C")
+richTEGtemperatureToFlashDrum = TEGprocess.getUnit("rich TEG heat exchanger 1").getOutStream(0).getTemperature("C")
+richTEGtemperatureToLPflashValve = TEGprocess.getUnit("rich TEG heat exchanger 2").getOutStream(0).getTemperature("C")
+richTEGtemperatureOutOfLPflashValve = TEGprocess.getUnit("Rich TEG LP flash valve").getOutStream().getTemperature("C")
+waterInGasFromCondenser= TEGprocess.getUnit("TEG regeneration column").getCondenser().getGasOutStream().getFlowRate("kg/hr")*TEGprocess.getUnit("TEG regeneration column").getCondenser().getGasOutStream().getFluid().getPhase(0).getWtFrac("water")
+
 		
 
 

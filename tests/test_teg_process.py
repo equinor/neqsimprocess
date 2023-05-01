f4e3c66e9fae60c2b2bec6d72b48cf8d47ac2910
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-05-01 13:01:23 +0000

    update

--- tests/test_teg_process.py
+++ tests/test_teg_process.py
@@ -1,2 +1,12 @@
+from neqsim.process.processTools import (compsplitter, waterDewPointAnalyser, hydrateEquilibriumTemperatureAnalyser, virtualstream, clearProcess, newProcess, runProcess, stream, runProcessAsThread, mixer, compressor, recycle2, splitter, valve)
+from neqsim.thermo import (TPflash, fluid, printFrame)
+from numpy import isnan
+from pytest import approx
+from jpype.types import *
+
+
 def test_Viscosity():
     assert abs(1.0760998263782569e-015) < 1e-10
+    
+def test_teg_process():
+    
